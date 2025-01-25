b906d1262250a598df1bfa8d8eb129bbec9b9c26
sanath <sanath6666bk@gmail.com>
2025-01-25 20:45:13 +0530

    Updated

--- hashing.c
+++ hashing.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 #define MAX_EMPLOYEES 10
 
-typedef struct employee {
+typedef struct {
     int id;
     char name[20];
     float salary;
@@ -11,82 +10,74 @@ typedef struct employee {
 Employee employees[MAX_EMPLOYEES];
 int hashTable[MAX_EMPLOYEES], employeeCount = 0;
 
-int addEmployee(int hashKey, int empID) {
-    FILE* file = fopen("open.txt", "a+");
-
-    employees[hashKey].id = empID;
-    printf("\nEnter employee name and salary: ");
-    scanf("%s %f", employees[hashKey].name, &employees[hashKey].salary);
-
-    fprintf(file, "%d\t%s\t%.2f\n", employees[hashKey].id, employees[hashKey].name, employees[hashKey].salary);
-    fclose(file);
-
-    return hashKey;
-}
-
-void displayHashTable() {
-    printf("\nHash Table:");
-    printf("\nKey\tID\tName\t\tSalary");
-    for (int i = 0; i < MAX_EMPLOYEES; i++) {
-        if (hashTable[i] != -1) {
-            printf("\n%d\t%d\t%-15s\t%.2f", i, employees[i].id, employees[i].name, employees[i].salary);
-        }
-    }
-}
-
-void handleCollision(int hashKey, int empID) {
-    int currentIndex = hashKey;
-
-    if (employeeCount == MAX_EMPLOYEES) {
-        printf("\nError: Hash table is full. Cannot add more employees.\n");
-        return;
-    }
-
-    if (hashTable[hashKey] == -1) {
-        hashTable[hashKey] = addEmployee(hashKey, empID);
-        employeeCount++;
-        displayHashTable();
-    } else {
-        printf("\nCollision detected at key %d. Resolving using linear probing...\n", hashKey);
-        while (hashTable[currentIndex] != -1) {
-            currentIndex = (currentIndex + 1) % MAX_EMPLOYEES;
-        }
-        hashTable[currentIndex] = addEmployee(currentIndex, empID);
-        employeeCount++;
-        printf("\nCollision resolved! Updated hash table:\n");
-        displayHashTable();
-    }
-}
-
-int main() {
-    int hashKey, empID, continueInput = 1;
-
+void initializeHashTable() {
     for (int i = 0; i < MAX_EMPLOYEES; i++) {
         hashTable[i] = -1;
     }
+}
 
-    printf("Welcome to the Employee Management System using Hashing\n");
+void displayHashTable() {
+    printf("\nHash Table:\nKey\tID\tName\t\tSalary\n");
+    for (int i = 0; i < MAX_EMPLOYEES; i++) {
+        if (hashTable[i] != -1) {
+            printf("%d\t%d\t%-15s\t%.2f\n", i, employees[i].id, employees[i].name, employees[i].salary);
+        }
+    }
+}
+
+void addEmployee(int index, int empID) {
+    printf("\nEnter Employee Name: ");
+    scanf("%s", employees[index].name);
+    printf("\nEnter Employee '%s' Salary: ");
+    scanf("%f", &employees[index].salary);
+    employees[index].id = empID;
+
+    FILE *file = fopen("open.txt", "a+");
+    fprintf(file, "%d\t%s\t%.2f\n", empID, employees[index].name, employees[index].salary);
+    fclose(file);
+
+    hashTable[index] = empID;
+    employeeCount++;
+    displayHashTable();
+}
+
+void handleCollision(int hashKey, int empID) {
+    if (employeeCount == MAX_EMPLOYEES) {
+        printf("Error: Hash table is full.\n");
+        return;
+    }
+
+    int index = hashKey;
+    while (hashTable[index] != -1) {
+        index = (index + 1) % MAX_EMPLOYEES;
+    }
+    addEmployee(index, empID);
+}
+
+int main() {
+    int empID, continueInput;
+
+    initializeHashTable();
+    printf("Employee Management System using Hashing\n");
 
     do {
         printf("\nEnter the employee ID (positive integer): ");
         scanf("%d", &empID);
 
-        hashKey = empID % MAX_EMPLOYEES;
-        handleCollision(hashKey, empID);
+        int hashKey = empID % MAX_EMPLOYEES;
+        if (hashTable[hashKey] == -1) {
+            addEmployee(hashKey, empID);
+        } else {
+            printf("Collision at key %d. Resolving...\n", hashKey);
+            handleCollision(hashKey, empID);
+        }
 
-        printf("\nDo you want to continue adding employees? (1=Yes, 0=No): ");
+        printf("Continue adding employees? (1=Yes, 0=No): ");
         scanf("%d", &continueInput);
     } while (continueInput);
 
     printf("\nFinal Hash Table:\n");
     displayHashTable();
 
-    printf("\nKeys in hash table: ");
-    for (int i = 0; i < MAX_EMPLOYEES; i++) {
-        if (hashTable[i] != -1) {
-            printf("%d ", hashTable[i]);
-        }
-    }
-    printf("\n");
     return 0;
 }
